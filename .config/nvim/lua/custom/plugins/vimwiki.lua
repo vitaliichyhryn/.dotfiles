2151a503fb82b97168d53d8631b7e7214bf20506
Vitalii Chyhryn <chyhryn.vitalii@gmail.com>
2025-05-18 22:50:43 +0300

    Add vimwiki plugin

---
+++ .config/nvim/lua/custom/plugins/vimwiki.lua
@@ -0,0 +1,13 @@
+return {
+  'vimwiki/vimwiki',
+  init = function()
+    vim.g.vimwiki_list = {
+      {
+        path = '~/notes/',
+        syntax = 'markdown',
+        ext = '.md',
+      },
+    }
+    vim.g.vimwiki_global_ext = 0
+  end,
+}
